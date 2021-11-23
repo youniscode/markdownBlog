0f3b74c8503ab5ad2203470b86d9bbe87ab89801
Younis <youniscode@gmail.com>
2021-11-23 16:39:10 +0100

    projectMarkdownBlogExpressNode.jsMongoDB

---
+++ models/article.js
@@ -0,0 +1,22 @@
+// Setting up the database
+const mongoose = require('mongoose')
+
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  markdown: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Article', articleSchema)
